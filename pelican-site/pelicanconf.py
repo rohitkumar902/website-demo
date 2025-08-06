d03ce2f37381569af6ba98453d2948be6e3f16ed
rohitkumar902 <anjaiswal9@gmail.com>
2025-08-06 11:01:09 +0530

    Create pelicanconf.py

---
+++ pelican-site/pelicanconf.py
@@ -0,0 +1,17 @@
+AUTHOR = 'Your Name'
+SITENAME = 'Saurabh Heights'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+DEFAULT_LANG = 'en'
+
+THEME = 'notebook'  # You’ll set this up locally
+
+PAGE_PATHS = ['pages']
+ARTICLE_PATHS = ['articles']
+
+STATIC_PATHS = ['images']
+
+DEFAULT_PAGINATION = 5
